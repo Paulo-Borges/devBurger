ea53d62a3d4643418d543e5d86841665e0bf3ef9
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-10-08 19:14:27 -0300

    feat: inserting routes in the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <Link to="/">App</Link>
+      <Link to="/bebidas">Bebidas</Link>
+      <Link to="/contato">Contato</Link>
+      <Link to="/lanches">Lanches</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/sacola">Sacola</Link>
+      <Link to="/signup">Signup</Link>
+    </nav>
+  );
 };
 
 export default Navbar;
