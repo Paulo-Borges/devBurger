f1f4216b7cacaf647464272b1f5468307b52c8b4
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-10-15 17:36:52 -0300

    feat: adicionando routes Contato

--- src/routes/Contato.tsx
+++ src/routes/Contato.tsx
@@ -2,17 +2,27 @@ const Contato = () => {
   return (
     <section>
       <h1 className="text-base py-4 text-xl ml-5">Contato</h1>
-      <form className="flex flex-col ml-12">
+      <form className="flex flex-col ml-12 space-y-4 ">
         <label htmlFor="">Nome:</label>
-        <input type="text" placeholder="Digite seu nome" />
+        <input
+          type="text"
+          placeholder="Digite seu nome"
+          className="border border-gray-200"
+        />
         <label htmlFor="">Email:</label>
-        <input type="email" placeholder="Digite seu email" />
+        <input
+          type="email"
+          placeholder="Digite seu email"
+          className="border border-gray-200"
+        />
         <label htmlFor="">Assunto:</label>
-        <input type="text" />
+        <input type="text" className="border border-gray-200" />
         <label htmlFor="">Mensagem</label>
-        <textarea className=""></textarea>
+        <textarea className="border border-gray-200"></textarea>
       </form>
-      <button>Enviar</button>
+      <div className="flex justify-end items-end">
+        <button className="bg-base text-white px-2 mb-20 mt-10">Enviar</button>
+      </div>
     </section>
   );
 };
